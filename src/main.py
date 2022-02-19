111c54cecf997b6c6b60339123f94ad8e66e43a4
Bea Maia <beamaia.work@gmail.com>
2022-02-19 20:41:59 -0300

    :construction: calculates current distance

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
-from platform import node
 import sys
 from copy import copy
 import numpy as np
 
 from read_input import *
-from graph import *
 from solution import *
-from utils import *
+from graph import *
 
-def show_route(solution: list[np.array], nodes:np.array) -> None:
+def show_route(sol: list[np.array], nodes:np.array) -> None:
     dist = 0
-    for i, route in enumerate(solution):
+    for i, route in enumerate(sol):
         route = route.flatten()
         print(f'Route #{i+1} - ', end="")
         print(*route.astype(int), sep=" ")
-        print(f'Total Distance: {total_distance(route, nodes)} - Capacity: {sum_route_capacity(route, nodes)}', end='\n\n')
+        print(f'Capacity: {sum_route_capacity(route, nodes)}', end='\n')
+        #print(f'Total Distance: {total_distance(route, nodes)}')
 
 def algorithm(file):
     nodes, vehicles, clients, vehicle_capacity = read_input(file)
@@ -23,8 +22,11 @@ def algorithm(file):
 
     current_sol = create_initial_sol(nodes, sorted_nodes, vehicles, vehicle_capacity)
     best_sol = copy(current_sol)
-
+    current_sol_dist = total_distance(current_sol, nodes)
+    best_sol_dist = total_distance(best_sol, nodes)
+    
     show_route(current_sol, nodes)
+    print(f"Current distance: {current_sol_dist}")
 
 if __name__ == '__main__':
     args = sys.argv
@@ -33,4 +35,6 @@ if __name__ == '__main__':
         algorithm(args[1])
     else:
         print("File not informed")
+        algorithm('input\A-n32-k5.vrp')
+
     
