71f0f6fe4bda3db8f0f91bd2d9fff0e0df1fb4e7
AHalic <sophie.gama@aluno.ufes.br>
2022-02-24 19:18:10 -0300

    adds shift (work in progress) and distance penalties
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- src/solution.py
+++ src/solution.py
@@ -2,6 +2,7 @@ from typing import List, Tuple
 
 import numpy as np
 import random
+from math import ceil 
 
 from graph import sum_route_capacity, route_distance, total_distance
 
@@ -9,7 +10,7 @@ ITER_SIZE = 100
 
 def sort_with_key(routes: List[dict], key: str, reverse:bool=True) -> List[dict]:
     """
-    Ordena lista de dicionario de forma descendente com base no string utilizada
+    Ordena lista de dicionario de forma descresncete com base no string utilizada
     Caso reverse=False, os valores estarao em ordem crescente.
     """
     return sorted(routes, key=lambda item: item[key], reverse=reverse) 
@@ -43,6 +44,28 @@ def calculate_savings(nodes: List[dict], dist: np.array, clients: int) -> List[d
                 best_savings.append(aux)
     
     return best_savings
+
+"""
+(8, 11)
+
+8 2 3 1
+11 5 6 7
+1 3 2 8 11 5 6 7
+
+8 2 3 1
+7 5 6 11
+7 5 6 11 8 2 3 1
+
+1 2 3 8
+7 5 6 11
+1 2 3 8 11 6 5 7
+
+1 2 3 8
+11 5 6 7
+1 2 3 8 11 5 6 7
+
+"""
+
 
 def join_route(route1, route2, value1, value2):
     """
@@ -79,13 +102,14 @@ def savings_initial_sol(dist: np.array, nodes:  List[dict], vehicles: int, clien
     best_savings = calculate_savings(nodes, dist, clients)
 
     # Ordena as economias por capacidade e o valor da economia, maior pro menor
-    best_savings = sort_with_key(best_savings, 'capacity')
+    # best_savings = sort_with_key(best_savings, 'capacity', reverse=False)
     best_savings = sort_with_key(best_savings, 'saving')
 
     # Cria n rotas, com n = clients
     solution = create_n_routes(clients)
 
     # Junta rotas ate quantidade de rotas == veiculos. Permite penalidade
+    savings_penalty = []
     while len(solution) > vehicles:
         # Tupla de economia, com o valor da economia e as cidades que pertence
         saving_tuple = best_savings.pop(0)
@@ -115,7 +139,7 @@ def savings_initial_sol(dist: np.array, nodes:  List[dict], vehicles: int, clien
         # Une cidades
         route = join_route(route_i[0], route_j[0], i, j)
 
-        # A rota unida esta vazia ou ultrapassou 20% da capacidade
+        # A rota unida esta vazia ou ultrapassou penalidade% da capacidade
         if not len(route) or sum_route_capacity(route, nodes) > (limit * (1 + penalidade)):
             continue
         
@@ -162,26 +186,6 @@ def random_initial_sol(nodes: List[dict], sorted_nodes: List[dict], vehicles:int
             
     return solution
 
-#TO DO:
-# Na busca de soluções:
-# Faz o vizinho se n é tabu -> verifica se é melhor q o vizinho atual
-#   se é tabu -> calcula a distancia e ve se é melhor q a melhor solução (aspiration)
-#  apenas armazena a melhor e o atual, as distancias e o movimento tabu
-# Movimentos -> primeiro y swaps (escolher melhor movimento)
-# Verificar tempo para swap com:
-#   - todas possibilidades de swap
-#   - x iterações, com x variando entre x1 = 10 x2 = 50 x3 = 100 x4 = 1000
-# Melhor solução atual sempre é o resultado da busca de vizinhaça
-# Melhor solução geral precisa ser sempre compara a melhor solução atual
-
-# Lista tabu:
-# Tupla: (cidade1, rota_destino), ([c1, c2], rt1, rt2), (rt1, rt2), {c1, c2}
-#      - escolher só (c1, c2)
-# Tenure: 15, static
-
-# Critério de parada:
-# - Tempo max: 300s
-# - x iterações sem modificação. x1 = 10 x2 = 50 x3 = 100 x4 = 1000
 
 def swap(solution, i_r1=-1, i_r2=-1, i_c1=-1, i_c2=-1, nodes=None):
     # new_solution = solution.copy()
@@ -205,82 +209,131 @@ def swap(solution, i_r1=-1, i_r2=-1, i_c1=-1, i_c2=-1, nodes=None):
 
     city1 = route1[i_c1]
     city2 = route2[i_c2]
+    # print('swap:', city1, city2)
 
     # Swap and create tabu move and solution
     route1[i_c1], route2[i_c2] = city2, city1
     new_solution[i_r1], new_solution[i_r2] = route1, route2
 
-    dist_aux = total_distance(solution, nodes) - route_distance(solution[i_r1], nodes) - route_distance(solution[i_r2], nodes) 
-    # print('\nno swaap dist: ', dist_aux)
-    dist_aux += route_distance(new_solution[i_r1], nodes) + route_distance(new_solution[i_r2], nodes) 
-    # print('result dist: ', dist_aux)
-
     tabu_move = set([city1, city2])
 
-    return new_solution, tabu_move, (i_r1, i_r2)
-
-
-
-    # dist_aux = dist_sol - route_distance(route1, nodes) - route_distance(route2, nodes) 
-    # dist_aux += route_distance(route1, nodes) + route_distance(route2, nodes) 
-    # print('dist', dist_aux) 
+    return new_solution, tabu_move
     
-# No best neighbor -> realiza o swap calcula para a nova solução a nova distancia e
-# a capacidade das rotas modificadas, se a capacidade for maior então refaz o movimento
+def shift(solution, r_o, r_d, i_c):
+    new_solution = [np.copy(route) for route in solution]
+    new_solution[r_d] = np.concatenate((solution[r_d], solution[r_o][i_c]),axis=None)
+    new_solution[r_o] = np.delete(new_solution[r_o], i_c, axis=None)
+    tabu_move = (solution[r_o][i_c])
+    return new_solution, tabu_move
+
 
 def best_neighbor(solution, dist_sol, nodes, max_routes, capacity, tabu_list, tenure):
-    # swap cidades 1 e 9
-    # swap {c1, c2}
-    # lista tabu
-    # 10 rotas
-    # 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3+ 2 + 1 = 55 swaps
-    # 10 interswaps, 45 outerswaps
-
-    # 4 rotas
-    # 4 + 3 + 2 + 1 = 10 swaps
-    # 4 interswaps, 6 outerswaps
-
-    # shift {c3}
-    # {c1}
-    # dist_sol = total_distance(solution, nodes)
-    current_sol, current_dist = None, None
-    
+    current_sol_swap, current_dist_swap = None, None
+    count_swaps = 0
     for i in range(0, max_routes):
+        # print(int(ceil(len(solution[i]) / 2)))
+        c1_indexes = random.sample(range(len(solution[i])), k=int(ceil(len(solution[i]) / 2)))
+        # print('c1', c1_indexes)
         for j in range(i, max_routes):
             # Realiza o movimento swap
-            aux_solution, movement, _ = swap(solution, i, j, nodes=nodes)
-            # show_route(aux_solution, nodes)
-            # print()
+            # print('r2', solution[j])
+            for i_c1 in c1_indexes:
+                for i_c2 in range(len(solution[j])):
+                    count_swaps += 1
+                    aux_solution, movement = swap(solution, i, j, i_c1, i_c2, nodes=nodes)
+                    # show_route(aux_solution, nodes)
+                    # print()
 
-            # Calcula a capacidade das novas rotas
-            if sum_route_capacity(aux_solution[i], nodes) > capacity or sum_route_capacity(aux_solution[j], nodes) > capacity:
-                continue
-            
-            # Calcula a distancia das novas rotas
-            if i != j:
+                    # Calcula a capacidade das novas rotas
+                    penalty_r1 = 1
+                    penalty_r2 = 1
+                    capacity_r1 = sum_route_capacity(aux_solution[i], nodes)
+                    capacity_r2 = sum_route_capacity(aux_solution[j], nodes)
+                    
+                    if capacity_r1 > capacity: 
+                        penalty_r1 += (capacity_r1 - capacity) / capacity 
+                    if capacity_r2 > capacity:
+                        penalty_r2 += (capacity_r2 - capacity) / capacity 
+
+
+                    
+                    # Calcula a distancia das novas rotas
+                    if i != j:
+                        dist_aux = dist_sol - route_distance(solution[i], nodes) - route_distance(solution[j], nodes) 
+                        dist_aux += route_distance(aux_solution[i], nodes) * penalty_r1 + route_distance(aux_solution[j], nodes) * penalty_r2
+                    if i == j:
+                        dist_aux = dist_sol - route_distance(solution[i], nodes)
+                        dist_aux += route_distance(aux_solution[i], nodes) * penalty_r1
+                    # print('\ndist: ', dist_aux, 'sol 1', route_distance(solution[i], nodes), 'sol2', route_distance(solution[j], nodes) )
+                    # print('result dist: ', dist_aux)
+
+                    # atualiza a solução atual da vizinhança para a primeira vez que acha 
+                    if current_sol_swap == None:
+                        current_sol_swap = aux_solution.copy()
+                        current_dist_swap = dist_aux
+                        current_move_swap = movement
+                    elif dist_aux < current_dist_swap:
+                        if movement not in tabu_list or dist_aux < dist_sol:
+                            current_sol_swap = aux_solution.copy()
+                            current_dist_swap = dist_aux
+                            current_move_swap = movement
+
+
+    count_shifts = 0
+    current_sol_shift, current_dist_shift = None, None
+    for i in range(0, max_routes):
+        # Não faz shift se a rota só tem um elemento 
+        if len(solution[i]) == 1:
+            continue
+        for j in range(i + 1, max_routes):
+            for i_c in range(len(solution[i])):
+                count_shifts += 1
+
+                aux_solution, movement = shift(solution, i, j, i_c1)
+                    
+
+                penalty_r1 = 1
+                penalty_r2 = 1
+                capacity_r1 = sum_route_capacity(aux_solution[i], nodes)
+                capacity_r2 = sum_route_capacity(aux_solution[j], nodes)
+                
+                if capacity_r1 > capacity: 
+                    penalty_r1 += (capacity_r1 - capacity) / capacity 
+                if capacity_r2 > capacity:
+                    penalty_r2 += (capacity_r2 - capacity) / capacity 
+
+                # Calcula a distancia das novas rotas
                 dist_aux = dist_sol - route_distance(solution[i], nodes) - route_distance(solution[j], nodes) 
-            if i == j:
-                dist_aux = dist_sol - route_distance(solution[i], nodes)
-            # print('\ndist: ', dist_aux, 'sol 1', route_distance(solution[i], nodes), 'sol2', route_distance(solution[j], nodes) )
-            dist_aux += route_distance(aux_solution[i], nodes) + route_distance(aux_solution[j], nodes) 
-            # print('result dist: ', dist_aux)
+                dist_aux += route_distance(aux_solution[i], nodes) *penalty_r1 + route_distance(aux_solution[j], nodes) * penalty_r2
 
-            # atualiza a solução atual da vizinhança
-            if current_sol == None:
-                current_sol = aux_solution.copy()
-                current_dist = dist_aux
-            elif dist_aux < current_dist:
-                if movement not in tabu_list or dist_aux < dist_sol:
-                    current_sol = aux_solution.copy()
-                    current_dist = dist_aux
-    
-    if current_sol != None:
-        # TODO tem q verificar o tamanho da lista tabu e o tenure
+                # atualiza a solução atual da vizinhança
+                if current_sol_shift == None:
+                    current_sol_shift = aux_solution.copy()
+                    current_dist_shift = dist_aux
+                    current_move_shift = movement
+                elif dist_aux < current_dist_shift:
+                    if movement not in tabu_list or dist_aux < dist_sol:
+                        current_sol_shift = aux_solution.copy()
+                        current_dist_shift = dist_aux
+                        current_move_shift = movement
+
+    # print('shift')
+    # print(current_sol_shift, current_dist_shift)
+    # print('swap')
+    # print(current_sol_swap, current_dist_swap)
+
+    if current_dist_swap < current_dist_shift:
         if len(tabu_list) == tenure:
             tabu_list.pop(0)
-        tabu_list.append(movement)
+        tabu_list.append(current_move_swap)
+        return current_sol_swap, current_dist_swap, tabu_list
+    else:
+        if len(tabu_list) == tenure:
+            tabu_list.pop(0)
+        tabu_list.append(current_move_shift)
+        return current_sol_shift, current_dist_shift, tabu_list
+        
 
-    return current_sol, current_dist, tabu_list
 
     
 
@@ -298,3 +351,24 @@ def best_neighbor(solution, dist_sol, nodes, max_routes, capacity, tabu_list, te
 # - melhor solucao da vizinhança vira a current_solucao
 # - tempo para delimitar o algoritmo
 # - decidir outras condicoes paradas 
+
+# TODO
+# Na busca de soluções:
+# Faz o vizinho se n é tabu -> verifica se é melhor q o vizinho atual
+#   se é tabu -> calcula a distancia e ve se é melhor q a melhor solução (aspiration)
+#  apenas armazena a melhor e o atual, as distancias e o movimento tabu
+# Movimentos -> primeiro y swaps (escolher melhor movimento)
+# Verificar tempo para swap com:
+#   - todas possibilidades de swap
+#   - x iterações, com x variando entre x1 = 10 x2 = 50 x3 = 100 x4 = 1000
+# Melhor solução atual sempre é o resultado da busca de vizinhaça
+# Melhor solução geral precisa ser sempre compara a melhor solução atual
+
+# Lista tabu:
+# Tupla: (cidade1, rota_destino), ([c1, c2], rt1, rt2), (rt1, rt2), {c1, c2}
+#      - escolher só (c1, c2)
+# Tenure: 15, static
+
+# Critério de parada:
+# - Tempo max: 300s
+# - x iterações sem modificação. x1 = 10 x2 = 50 x3 = 100 x4 = 1000
